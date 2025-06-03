d8b269efe5f5943e90188c1bca9c73b0b9a85945
Ryan B <bombingrun3@gmail.com>
2025-06-02 19:02:56 -0500

    Added buttons for video gallery and Contact

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,6 +8,14 @@ function Main() {
       <div className="main__content_container">
         <h1 className="main__title">Actress Extraordinaire</h1>
         <p className="main__paragraph">TV, Film, and Theater</p>
+        <div className="main__button_container">
+          <button className="main__button_videos" type="button">
+            Videos
+          </button>
+          <button className="main__button_contact" type="button">
+            Contact
+          </button>
+        </div>
       </div>
     </main>
   );
